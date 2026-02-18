48dd358b7b97e90fa2f77e1346460a4c9efdfce6
Samuel Welborn <swelborn@stanford.edu>
2026-02-18 09:50:08 -0500

    claude-generate models based on db design

--- src/elogfetch/storage/__init__.py
+++ src/elogfetch/storage/__init__.py
@@ -3,5 +3,47 @@
 from __future__ import annotations
 
 from .database import Database, find_latest_database, generate_db_name
+from .models import (
+    PI,
+    Detector,
+    ElogContentType,
+    Experiment,
+    ExperimentAnalysisQueue,
+    ExperimentSlackChannel,
+    Logbook,
+    Metadata,
+    PosixGroup,
+    Questionnaire,
+    Run,
+    RunDetector,
+    RunProductionData,
+    User,
+    UserPosixGroup,
+    Workflow,
+    WorkflowDefinition,
+    WorkflowTriggerType,
+)
 
-__all__ = ["Database", "find_latest_database", "generate_db_name"]
+__all__ = [
+    "Database",
+    "find_latest_database",
+    "generate_db_name",
+    "Detector",
+    "ElogContentType",
+    "Experiment",
+    "ExperimentAnalysisQueue",
+    "ExperimentSlackChannel",
+    "Logbook",
+    "Metadata",
+    "PI",
+    "PosixGroup",
+    "Questionnaire",
+    "Run",
+    "RunDetector",
+    "RunProductionData",
+    "User",
+    "UserPosixGroup",
+    "Workflow",
+    "WorkflowDefinition",
+    "WorkflowTriggerType",
+]
